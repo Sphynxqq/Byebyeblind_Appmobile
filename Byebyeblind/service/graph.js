90d621d5640a83d38d7c5f82bf82701613a4b0c8
Plai <plai.rt.w@gmail.com>
2020-05-16 16:10:09 +0700

    Finish graph service

--- Byebyeblind/service/graph.js
+++ Byebyeblind/service/graph.js
@@ -1,36 +1,47 @@
 export async function getGraph(symbolName) {
-  /*
+  /* Data from API format
 [
     {"S_ID":1,
     "TICKER":"7UP",
-    "DATE":"20190822",
-    "OPEN":"0.51",
-    "HIGH":"0.52",
-    "LOW":"0.48",
-    "CLOSE":"0.48",
-    "VOL":"42962100"},
+    "DATE":"20190822", // string
+    "OPEN":"0.51", // string
+    "HIGH":"0.52", // string
+    "LOW":"0.48", // string
+    "CLOSE":"0.48", // string
+    "VOL":"42962100"}, // string
     ...
 ]
   */
   try {
     const raw = await fetch('http://192.168.1.37:3000/' + symbolName);
-    const parsedData = raw.json().map(({OPEN, CLOSE, HIGH, LOW, VOL, DATE}) => {
+    return raw.json().map(({OPEN, CLOSE, HIGH, LOW, VOL, DATE}) => {
       const year = Number.parseInt(DATE.slice(0, 4));
-      const month = DATE.slice(4, 6);
-      const date = DATE.slice(6, 8);
+      const month = Number.parseInt(DATE.slice(4, 6));
+      const date = Number.parseInt(DATE.slice(6, 8));
       return {
         open: Number.parseFloat(OPEN),
         close: Number.parseFloat(CLOSE),
         high: Number.parseFloat(HIGH),
         low: Number.parseFloat(LOW),
         vol: Number.parseFloat(VOL),
-        date: new Date(year, month, date),
+        date: new Date(year, month - 1, date),
       };
     });
-
-    return parsedData;
   } catch (error) {
     console.error('Request failed.', error);
     throw error;
   }
+  /*
+  Return data format
+  [
+    {
+      open: 1, // float
+      close: 1, // float
+      high: 1, // float
+      low: 1, // float
+      vol: 1, // float
+      date: Date(2020, 2, 12), // date
+    }
+  ]
+   */
 }
