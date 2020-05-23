1c90f11f8d5d4acbe65a0cd9bfb1235302a192f5
jirapatpao <jirapat-1230@hotmail.com>
2020-05-23 22:42:36 +0700

    update ดึงค่าแบบ week

--- Api/Routes.js
+++ Api/Routes.js
@@ -79,18 +79,18 @@ app.get('/7upmore', function (req, res) {
 
 app.get('/checkstock/:name', function (req, res) {
 
-  connection.getConnection(function (err, connection) {   
+  connection.getConnection(function (err, connection) {
     // console.log(req.params);
-    var query = ("SELECT COUNT(TICKER) AS 'CHECK' FROM ticker_name WHERE TICKER = '"+ req.params.name + "'");
+    var query = ("SELECT COUNT(TICKER) AS 'CHECK' FROM ticker_name WHERE TICKER = '" + req.params.name + "'");
     console.log("this is name : " + req.params.name);
     console.log("this is SQL : " + query);
     connection.query(query, function (error, result, fields) {
       var check = result[0].CHECK;
-        console.log(check);
-        if(check == 1)
-          check = true;
-        else
-          check = false;
+      console.log(check);
+      if (check == 1)
+        check = true;
+      else
+        check = false;
       if (error) throw error;
 
       res.send(check)
@@ -108,6 +108,7 @@ app.get('/getStock/day/:name', function (req, res) {
     connection.query(query, function (error, result, fields) {
       // console.log(result);
       // console.log(result[0]);
+
       if (error) {
         throw error;
       }
@@ -126,7 +127,46 @@ app.get('/getStock/week/:name', function (req, res) {
     var query = ("SELECT * FROM " + req.params.name + " LIMIT 700");
     // console.log("this is SQL : " + query);
     connection.query(query, function (error, result, fields) {
-      // console.log(result[0]);
+
+      console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< START >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+      var dateFirst = Number.parseInt(result[0].DATE.slice(6, 8), 10);
+      var dayCount = 0;
+      var daySum = 0;
+      var average = 0;
+      const result2 = result.map(({ S_ID, OPEN, CLOSE, HIGH, LOW, VOL, DATE }) => {
+        const year = Number.parseInt(DATE.slice(0, 4), 10);
+        const month = Number.parseInt(DATE.slice(4, 6), 10);
+        const date = Number.parseInt(DATE.slice(6, 8), 10);
+
+        if (date - dateFirst >= -6 && date - dateFirst <= 6) {
+          console.log("date : " + date + "/" + month + "/" + year);
+          console.log("sum++");
+          dayCount++;
+          daySum += Number.parseFloat(VOL);
+          return {
+            open: Number.parseFloat(OPEN),
+            close: Number.parseFloat(CLOSE),
+            high: Number.parseFloat(HIGH),
+            low: Number.parseFloat(LOW),
+            vol: Number.parseFloat(VOL),
+            date: new Date(year, month - 1, date),
+          };
+        } else {
+          average = daySum / dayCount;
+          console.log(daySum + " / " + dayCount + " = " + average);
+          console.log("average : " + average);
+
+          console.log("END WEEK ")
+          console.log("date : " + date + "/" + month + "/" + year);
+          dayCount = 1;
+          daySum = daySum = Number.parseFloat(VOL);
+          dateFirst = date;
+          console.log("datefirst : " + dateFirst);
+        }
+
+      })
+      // console.log(result2);
+
       if (error) {
         throw error;
       }
